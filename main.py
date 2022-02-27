d61e60ee1c5f75977399f8546e1fe6c006e59ca7
IADF <38521733+IADF@users.noreply.github.com>
2022-02-27 15:40:36 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import time
 import include
+import read_data
 
 if __name__ == '__main__':
+    #   读取表格数据
+    read_data.read_data()
 
+    #
+    start_time = time.time()
+
+    end_time = time.time()
 
